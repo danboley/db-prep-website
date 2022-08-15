cf8427a120f5cc465634b9a7ccf8688bbb9237f1
Daniel Boley <danboley1@gmail.com>
2022-08-15 17:38:18 -0400

    update js h2 to h4, change text

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const h4 = document.createElement("h4");
+h4.textContent = "Created by Daniel Boley for Flatiron School";
+document.querySelector("body").appendChild(h4);
