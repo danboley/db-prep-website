88f3ba6b71abb18e8ef866fbe65d8a1c7eb0aac5
Daniel Boley <danboley1@gmail.com>
2022-08-15 18:20:03 -0400

    added click me button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h4 = document.createElement("h4");
 h4.textContent = "Created by Daniel Boley for Flatiron School";
 document.querySelector("body").appendChild(h4);
+
+function popupButton () {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
